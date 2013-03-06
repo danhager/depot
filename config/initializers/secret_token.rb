6cba26fa7933c255b79a999f3efdf9c08df36895
danhager <dan.hager@starfieldtms.com>
2013-03-06 11:46:39 -0500

    Fresh Rails App Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'cb09806850c8f4d1e8d41c07a0dba4bb2eca6731fe2491e32586738329be31ccc3e486c3d2908b451772206a771a5efe721f7e123189c0e132ab16112b229cdc'
